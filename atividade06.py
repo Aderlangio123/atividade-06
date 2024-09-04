5bb284af2a33267c39ae2b2726371be8de4347d0
Aderlangio123 <faderlangio123@gmail.com>
2024-09-04 15:41:03 -0300

    atividade 06 - nome completo

---
+++ atividade06.py
@@ -0,0 +1,9 @@
+# Crie um programa que receba dois nomes (primeiro nome e sobrenome) e exiba o nome completo.
+
+nome = str(input("digite seu 1° nome! "))
+sobrenome = str(input("digite seu sobrenome! "))
+
+nomeCompleto = nome + " " + sobrenome 
+
+print ("seu nome completo é ", nomeCompleto)
+
